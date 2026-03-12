9a1785fa710087bcbe59be6f6e88ca88749c9e11
damagadi-create <damagadi@gmail.com>
2026-03-12 11:00:50 +0100

    Add files via upload

---
+++ job_bot.py
@@ -0,0 +1,89 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import json
+
+BOT_TOKEN = "8644838935:AAHCnQk96pNv-EFTqwsRL4LBTC2XzzvFGCQ"
+CHAT_ID = "8040018117"
+
+search_urls = {
+
+"LinkedIn Hamburg": "https://www.linkedin.com/jobs/search/?keywords=quality%20manager%20OR%20quality%20engineer%20OR%20qualit%C3%A4tsmanager%20OR%20qualit%C3%A4tsingenieur%20OR%20continuous%20improvement&location=Hamburg%2C%20Germany&f_TPR=r28800",
+
+"LinkedIn Remote": "https://www.linkedin.com/jobs/search/?keywords=quality%20manager%20OR%20quality%20engineer%20OR%20qualit%C3%A4tsmanager%20OR%20qualit%C3%A4tsingenieur%20OR%20continuous%20improvement&location=Germany&f_WT=2&f_TPR=r28800"
+
+}
+
+keywords = [
+"quality",
+"qualitäts",
+"iso",
+"continuous improvement",
+"operational excellence",
+"process improvement"
+]
+
+exclude_keywords = [
+"software",
+"qa tester",
+"automation tester",
+"selenium",
+"javascript",
+"developer",
+"software engineer",
+"test engineer"
+]
+
+try:
+    with open("sent_jobs.json", "r") as f:
+        sent_jobs = set(json.load(f))
+except:
+    sent_jobs = set()
+
+def send_telegram(text):
+
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    requests.post(url, data={
+        "chat_id": CHAT_ID,
+        "text": text,
+        "disable_web_page_preview": True
+    })
+
+def scan_jobs():
+
+    headers = {"User-Agent": "Mozilla/5.0"}
+
+    for site, url in search_urls.items():
+
+        r = requests.get(url, headers=headers)
+
+        soup = BeautifulSoup(r.text, "html.parser")
+
+        jobs = soup.select("a.base-card__full-link")
+
+        for job in jobs:
+
+            title = job.text.strip()
+            href = job["href"]
+
+            title_lower = title.lower()
+
+            if any(k in title_lower for k in keywords) and not any(x in title_lower for x in exclude_keywords):
+
+                if href not in sent_jobs:
+
+                    sent_jobs.add(href)
+
+                    with open("sent_jobs.json", "w") as f:
+                        json.dump(list(sent_jobs), f)
+
+                    message = f"{site} job\n\n{title}\n{href}"
+
+                    send_telegram(message)
+
+while True:
+
+    scan_jobs()
+
+    time.sleep(900)
